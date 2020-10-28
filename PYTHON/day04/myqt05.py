ec06f665f0f4074301001bb2502d25ed371d25b3
junyoung <junyeong.b2n2w@gmail.com>
2020-10-29 08:59:24 +0900

    omok_1

--- PYTHON/day04/myqt05.py
+++ PYTHON/day04/myqt05.py
@@ -13,43 +13,22 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
 
         #버튼에 기능을 연결하는 코드
-        self.bt1.clicked.connect(self.bt1f)
-        self.bt2.clicked.connect(self.bt2f)
-        self.bt3.clicked.connect(self.bt3f)
-        self.bt4.clicked.connect(self.bt4f)
-        self.bt5.clicked.connect(self.bt5f)
-        self.bt6.clicked.connect(self.bt6f)
-        self.bt7.clicked.connect(self.bt7f)
-        self.bt8.clicked.connect(self.bt8f)
-        self.bt9.clicked.connect(self.bt9f)
-        self.bt0.clicked.connect(self.bt0f)
+        self.bt1.clicked.connect(self.btf)
+        self.bt2.clicked.connect(self.btf)
+        self.bt3.clicked.connect(self.btf)
+        self.bt4.clicked.connect(self.btf)
+        self.bt5.clicked.connect(self.btf)
+        self.bt6.clicked.connect(self.btf)
+        self.bt7.clicked.connect(self.btf)
+        self.bt8.clicked.connect(self.btf)
+        self.bt9.clicked.connect(self.btf)
+        self.bt0.clicked.connect(self.btf)
         self.bt_call.clicked.connect(self.call)
        
-
-   
+    def btf(self):
+        self.line.setText(self.line.text() + self.sender().text())
         
-    def bt1f(self) :
-        self.line.setText(self.line.text() + "1")
-    def bt2f(self) :
-        self.line.setText(self.line.text() + "2")
-    def bt3f(self) :
-        self.line.setText(self.line.text() + "3")
-    def bt4f(self) :
-        self.line.setText(self.line.text() + "4")
-    def bt5f(self) :
-        self.line.setText(self.line.text() + "5")
-    def bt6f(self) :
-        self.line.setText(self.line.text() + "6")
-    def bt7f(self) :
-        self.line.setText(self.line.text() + "7")
-    def bt8f(self) :
-        self.line.setText(self.line.text() + "8")
-    def bt9f(self) :
-        self.line.setText(self.line.text() + "9")
-    def bt0f(self) :
-        self.line.setText(self.line.text() + "0")
     def call(self) :
-        
         QMessageBox.about(self,"전화걸어요 ~",self.line.text() +  "로 전화거는중")
 
 
